d8cbcff10cc7523dacafdc6d5302a141141028fb
Geovana Matias R <120607451+GeovanaMat@users.noreply.github.com>
2022-12-22 13:00:30 -0400

    Add files via upload

---
+++ 20222220006.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int contador,numero,y;
+
+    
+    
+    
+  for(y = 1; y < 10; y++)
+  {
+      printf(" tabuada do %d: ", y);
+ 
+      for (contador = 1; contador <10; contador++){
+      printf("%d ",y*contador);
+      
+}
+      printf("\n");
+  }
+    
+
+}
