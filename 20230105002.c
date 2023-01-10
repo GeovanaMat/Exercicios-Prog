d42e3afeb0a07ff5bed6562d09e012916f88619b
Geovana Matias R <120607451+GeovanaMat@users.noreply.github.com>
2023-01-10 00:05:53 -0300

    Update 20230105002.c

--- 20230105002.c
+++ 20230105002.c
@@ -4,14 +4,27 @@
 int peso,tempgest;
 
 int bebes() {
-    scanf("%d",peso,tempgest);
+    scanf("%d%d",&peso,&tempgest);
 
 }
 
 void validar(){
     if (peso < 100 || tempgest < 28){
-        printf ("Parto não deverá ser realizado, reavaliar clinicamente");
+        printf ("Parto nÃ£o deverÃ¡ ser realizado, reavaliar clinicamente");
 
+    }
+    else if (peso > 2500 && tempgest > 28){
+        printf("Parto normal");
+    }
+    else if(peso > 2500 && tempgest <= 28){
+
+        printf("Parto Cesariana");
+    }
+    else if (peso >= 1500 && peso <= 2000 && tempgest > 36){
+        printf("Parto normal");
+    }
+    else{
+        printf("Parto cesarina");
     }
 }
 
