cd867a6801ed60300ff2f5c170e1639983a253dc
Geovana Matias R <120607451+GeovanaMat@users.noreply.github.com>
2023-02-14 19:41:18 -0500

    Add files via upload

---
+++ 20221222_009.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int m,n,i,j,result;
+    printf("Escreva o numero de M e N");
+    scanf("%d%d",&m,&n);
+    for (i = 0; i <= m; i++){
+        for (j = 0; j <= n; j++){
+            result = ij - ii + j;
+            printf("resultado :%d \n",result);
+        }
+    }
+
+}
